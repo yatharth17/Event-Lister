92eac791a86d5e492cee75318b60c45363fa8a79
yatharth17 <yatharthkatyal4@gmail.com>
2020-05-01 20:17:09 +0530

    final-changes

--- src/Components/DashBoard/Event.js
+++ src/Components/DashBoard/Event.js
@@ -5,7 +5,7 @@ const Event=({event,index})=>{
    return(
     <div className='post bg-white p-1 my-1'>
     <h2> Event {index}</h2>
-    <h6 className="large text-primary">{event.text}</h6>
+    <h4 className=" text-primary">{event.text}</h4>
     
     </div>
    )
